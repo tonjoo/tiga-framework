4d7eccff25c692d2361fc70b2a82ea4376f5165b
todiadiyatmo <todiadiyatmo@tonjoo.com>
2015-06-12 17:26:42 +0700

    update

--- src/Whoops.php
+++ src/Whoops.php
@@ -14,17 +14,16 @@ class Whoops
 	{
 		if(TIGA_DEBUG==true&&!$this->app->isConsole()) 
 		{
+			
 			// @todo Load Whoops only in debug mode 
 			$whoops = new \Whoops\Run();
 
-			// var_dump($this->app['request']);die();
-
-			if($this->app['request']->isXmlHttpRequest())
+			if($this->app['request']->isJson())
 				$whoops->pushHandler(new \Whoops\Handler\JsonResponseHandler());
 			else
-				$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
-			
-			$this->app['whoops'] = $whoops;			
+				$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());			
+
+			$whoops->register();
 		}
 	}
 }
