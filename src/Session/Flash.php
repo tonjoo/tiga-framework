83e415337aaddab73bb272a2cb3d83308249eb2e
eringga <eringga23@gmail.com>
2016-02-17 17:56:52 +0700

    bug fix session flash

--- src/Session/Flash.php
+++ src/Session/Flash.php
@@ -1,131 +1,150 @@
 <?php
 
 namespace Tiga\Framework\Session;
-use Tiga\Framework\Session\Session as Session;
 
 /**
- * Handle flash message
- */ 
-class Flash {
+ * Handle flash message.
+ */
+class Flash
+{
+    /**
+     * @var session
+     */
+    private $session;
 
-	/**
-	 * @var session
-	 */ 
-	private $session;
+    /**
+     * Hold Flash Data.
+     *
+     * @var array
+     */
+    private $data = array();
 
-	/**
-	 * Hold Flash Data
-	 * @var array
-	 */
-	private $data = array();
+    /**
+     * Class constructor.
+     *
+     * @param Session $session
+     *
+     * @return Flash
+     */
+    public function __construct(Session $session)
+    {
+        $this->session = $session;
 
-	/**
-	  * Class constructor
-	  * @param Session $session 
-	  * @return Flash
-	  */ 
-	function __construct(Session $session)
-	{
-		$this->session = $session;
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Add object to flash bag.
+     *
+     * @param string $key
+     * @param mixed  $value
+     */
+    public function add($key, $value)
+    {
+        $this->session->getFlashBag()->add($key, $value);
+    }
 
-	/**
-	 * Add object to flash bag
-	 * @param string $key 
-	 * @param mixed $value 
-	 */	 
-	function add($key,$value) {
-		$this->session->getFlashBag()->add($key,$value);
-	}
+    /**
+     * Set flash object to according to key.
+     *
+     * @param string $key
+     * @param mixed  $value
+     */
+    public function set($key, $value)
+    {
+        $this->session->getFlashBag()->set($key, $value);
+    }
 
-	/**
-	 * Set flash object to according to key
-	 * @param string $key 
-	 * @param mixed $value 
-	 */ 
-	function set($key,$value) {
-		$this->session->getFlashBag()->set($key,$value);
-	}
+    /**
+     * Copy flash object into local $data. So it can be called repeately.
+     *
+     * @param string $key
+     */
+    public function populateFlash($key)
+    {
+        if (isset($this->data[$key])) {
+            return;
+        }
 
-	/**
-	 * Copy flash object into local $data. So it can be called repeately
-	 * @param string $key
-	 */ 
-	function populateFlash($key){
+        if ($this->session->getFlashBag()->has($key)) {
+            $flash = $this->session->getFlashBag()->get($key);
+            
+            if (sizeof($flash) == 1 && isset($flash[0])) {
+                $flash = @$flash[0];
+            }
 
-		if(isset($this->data[$key]))
-			return;
+            $this->data[$key] = $flash;
+        }
+    }
 
-		if($this->session->getFlashBag()->has($key))
-		{
-			$flash = $this->session->getFlashBag()->get($key);
+    /**
+     * Get object from flash bag.
+     *
+     * @param string $key
+     * @param mixed  $defaultValue
+     *
+     * @return mixed
+     */
+    public function get($key, $defaultValue = array())
+    {
+        $this->populateFlash($key);
 
-			if(sizeof($flash)==1)
-				$flash = $flash[0];
+        if ($this->has($key)) {
+            return $this->data[$key];
+        }
 
-			$this->data[$key] = $flash;
-		}
+        return $defaultValue;
+    }
 
-	}
+    /**
+     * Sets all flash messages.
+     *
+     * @param array $attributes
+     */
+    public function setAll($attributes)
+    {
+        $this->session->getFlashBag()->setAll($attributes);
+    }
 
-	/**
-	 * Get object from flash bag
-	 * @param string $key
-	 * @param mixed $defaultValue 
-	 * @return mixed
-	 */
-	function get($key,$defaultValue = array()) {
-			
-		$this->populateFlash($key);
+    /**
+     * Get all flash bag content.
+     *
+     * @return array
+     */
+    public function all()
+    {
+        return $this->session->getFlashBag()->peekAll();
+    }
 
-		if($this->has($key))
-			return $this->data[$key];
+    /**
+     * Check if flash bag has $key.
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function has($key)
+    {
+        $this->populateFlash($key);
 
-		return $defaultValue;
-	}
+        return array_key_exists($key, $this->data);
+    }
 
-	/**
-	 * Sets all flash messages.
-	 * @param array $attributes 
-	 */
-	function setAll($attributes) {
-		$this->session->getFlashBag()->setAll($attributes);
-	}
+    /**
+     * Return all list of defined keys.
+     */
+    public function keys()
+    {
+        return $this->session->getFlashBag()->keys();
+    }
 
-	/**
-	 * Get all flash bag content
-	 * @return array
-	 */
-	function all() {
-		return $this->session->getFlashBag()->peekAll();
-	}
-
-	/**
-	 * Check if flash bag has $key
-	 * @param string $key 
-	 * @return boolean
-	 */
-	function has($key) {
-		$this->populateFlash($key);
-
-		return array_key_exists($key,$this->data);
-	}
-
-	/**
-	 * Return all list of defined keys
-	 */
-	function keys() {
-		return $this->session->getFlashBag()->keys();
-	}
-
-	/**
-	 * Clear the flash bag
-	 * @return mixed
-	 */
-	function clear() {
-		return $this->session->getFlashBag()->clear();
-	}
-
+    /**
+     * Clear the flash bag.
+     *
+     * @return mixed
+     */
+    public function clear()
+    {
+        return $this->session->getFlashBag()->clear();
+    }
 }
